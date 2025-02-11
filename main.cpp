dc587fc292d3ff788b5f2aba5212d6cfb364f38d
srshishir04 <142996222+srshishir04@users.noreply.github.com>
2025-02-11 02:07:51 +0200

    CROW_ROUTE(app, /)

--- main.cpp
+++ main.cpp
@@ -8,18 +8,19 @@
 #include <string>
 #include <vector>
 
-const std::string test = "hello world";
-
 void debug(const char *s) { std::cout << "DEBUG: " << s << std::endl; }
 void debug(std::string s) { std::cout << "DEBUG: " << s << std::endl; }
 
-std::vector<unsigned char> generate_png() {
+std::vector<unsigned char> generate_png()
+{
   std::chrono::time_point start = std::chrono::high_resolution_clock::now();
 
   unsigned width = 256, height = 256;
   std::vector<unsigned char> image(width * height * 4);
-  for (unsigned y = 0; y < height; y++) {
-    for (unsigned x = 0; x < width; x++) {
+  for (unsigned y = 0; y < height; y++)
+  {
+    for (unsigned x = 0; x < width; x++)
+    {
       image[4 * width * y + 4 * x + 0] = x % 256;
       image[4 * width * y + 4 * x + 1] = y % 256;
       image[4 * width * y + 4 * x + 2] = 128;
@@ -28,7 +29,8 @@ std::vector<unsigned char> generate_png() {
   }
   std::vector<unsigned char> png;
   unsigned error = lodepng::encode(png, image, width, height);
-  if (error) {
+  if (error)
+  {
     std::cerr << "encoder error " << error << ": " << lodepng_error_text(error)
               << std::endl;
   }
@@ -41,9 +43,11 @@ std::vector<unsigned char> generate_png() {
   return png;
 }
 
-int create_database(sqlite3 *&db) {
+int create_database(sqlite3 *&db)
+{
   int rc = sqlite3_open("example.db", &db);
-  if (rc) {
+  if (rc)
+  {
     std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
     return rc;
   }
@@ -52,40 +56,56 @@ int create_database(sqlite3 *&db) {
                     "AUTOINCREMENT, Data BLOB NOT NULL);";
   char *errMsg = nullptr;
   rc = sqlite3_exec(db, sql, nullptr, nullptr, &errMsg);
-  if (rc != SQLITE_OK) {
+  if (rc != SQLITE_OK)
+  {
     std::cerr << "SQL error: " << errMsg << std::endl;
     sqlite3_free(errMsg);
   }
   return rc;
 }
 
-int main() {
+int main()
+{
   crow::SimpleApp app;
   sqlite3 *db = nullptr;
 
-  if (create_database(db) != SQLITE_OK) {
+  if (create_database(db) != SQLITE_OK)
+  {
     return 1;
   }
 
-  CROW_ROUTE(app, "/generate/image")([]() {
+  CROW_ROUTE(app, "/generate/image")([]()
+                                     {
     crow::response res;
     res.set_header("Connection", "Close");
     res.set_header("Content-Type", "image/png");
     auto png = generate_png();
     res.write(std::string(png.begin(), png.end()));
-    return res;
-  });
-  CROW_ROUTE(app, "/")([]() {
+    return res; });
+  // CROW_ROUTE(app, "/")([]() {
+  //   auto page = crow::mustache::load("index.html");
+  //   std::string name = "Giovanni";
+  //   crow::mustache::context ctx({{"user", name}});
+  //   crow::response res(page.render(ctx));
+  //   res.set_header("Connection", "Close");
+  //   return res;
+  // });
+
+  CROW_ROUTE(app, "/")([]()
+                       {
     auto page = crow::mustache::load("index.html");
-    std::string name = "Giovanni";
-    crow::mustache::context ctx({{"user", name}});
+
+    crow::mustache::context ctx;
+    ctx["user"] = "Giovanni"; // Store key-value pairs in context directly
+
     crow::response res(page.render(ctx));
     res.set_header("Connection", "Close");
-    return res;
-  });
+
+    return res; });
 
   CROW_ROUTE(app, "/upload")
-      .methods(crow::HTTPMethod::POST)([](const crow::request &req) {
+      .methods(crow::HTTPMethod::POST)([](const crow::request &req)
+                                       {
         auto body = req.body;
         crow::json::rvalue formData = crow::json::load(body);
         // debug("form data: " + std::string(formData));
@@ -105,10 +125,9 @@ int main() {
 
         auto res = crow::response(os.str());
         res.set_header("Connection", "Close");
-        return res;
-      });
+        return res; });
 
-  app.port(8080).timeout(10).multithreaded().run();
+  app.port(8080).multithreaded().run();
 
   sqlite3_close(db);
   return 0;
